e56d7296049543b2c00e4abb2710cce6c60c8af3
mengyuan tu <mengyuan@mengyuans-MacBook.local>
2016-07-31 17:26:38 -0700

    code formated

--- app/src/main/java/com/mengyuantu/chordsquiz/MainActivity.java
+++ app/src/main/java/com/mengyuantu/chordsquiz/MainActivity.java
@@ -15,61 +15,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private int score(boolean questionOneRight, boolean questionTwoRightOne, boolean questionTwoRightTwo,boolean questionTwoRightThree, boolean questionThreeRight, boolean questionFourRight, int questionFiveRight){
+
+    private int score(boolean questionOneRight, boolean questionTwoRightOne, boolean questionTwoRightTwo, boolean questionTwoRightThree, boolean questionThreeRight, boolean questionFourRight, int questionFiveRight) {
 
         int baseScore = 0;
 
-        if (questionOneRight){
-            baseScore ++;
+        if (questionOneRight) {
+            baseScore++;
         }
-        if (questionTwoRightOne && questionTwoRightTwo && !questionTwoRightThree){
-            baseScore ++;
+        if (questionTwoRightOne && questionTwoRightTwo && !questionTwoRightThree) {
+            baseScore++;
         }
-        if (questionThreeRight){
-            baseScore ++;
+        if (questionThreeRight) {
+            baseScore++;
         }
-        if (questionFourRight){
-            baseScore ++;
+        if (questionFourRight) {
+            baseScore++;
         }
-        if (questionFiveRight == 6){
-            baseScore ++;
+        if (questionFiveRight == 6) {
+            baseScore++;
         }
         return baseScore * 20;
     }
-    private String createSummary(int score, boolean questionOneRight, boolean questionTwoRightOne, boolean questionTwoRightTwo,boolean questionTwoRightThree,boolean questionThreeRight, boolean questionFourRight, int questionFiveRight){
+
+    private String createSummary(int score, boolean questionOneRight, boolean questionTwoRightOne, boolean questionTwoRightTwo, boolean questionTwoRightThree, boolean questionThreeRight, boolean questionFourRight, int questionFiveRight) {
         String summary = "Question one's answer is F chord";
-        if (questionOneRight){
+        if (questionOneRight) {
             summary += "\n" + "Your answer is right";
-        }
-        else {
+        } else {
             summary += "\n" + "Your anwer is wrong";
         }
         summary += "\n" + "Question two's answer is A chord and E chord";
-        if (questionTwoRightOne && questionTwoRightTwo &&! questionTwoRightThree){
-            summary += "\n" +"Your anwer is right";
-        }
-        else{
+        if (questionTwoRightOne && questionTwoRightTwo && !questionTwoRightThree) {
+            summary += "\n" + "Your anwer is right";
+        } else {
             summary += "\n" + "Your anwer is wrong";
         }
         summary += "\n" + "Question Three's answer is Bm chord";
-        if (questionThreeRight){
+        if (questionThreeRight) {
             summary += "\n" + "Your answer is right";
-        }
-        else {
+        } else {
             summary += "\n" + "Your answer is wrong";
         }
         summary += "\n" + "Question Four's answer is G chord";
-        if (questionFourRight){
+        if (questionFourRight) {
             summary += "\n" + "Your answer is right";
-        }
-        else {
+        } else {
             summary += "\n" + "Your answer is wrong";
         }
         summary += "\n" + "Question Five's answer is 6";
-        if (questionFiveRight == 6){
+        if (questionFiveRight == 6) {
             summary += "\n" + "Your answer is right";
-        }
-        else {
+        } else {
             summary += "\n" + "Your answer is wrong";
         }
         summary += "\n" + "Your total score is " + score;
@@ -77,33 +74,34 @@ public class MainActivity extends AppCompatActivity {
         return summary;
 
     }
-    public void submit(View view){
 
-        RadioButton questionOne = (RadioButton)findViewById(R.id.question_one_answer);
+    public void submit(View view) {
+
+        RadioButton questionOne = (RadioButton) findViewById(R.id.question_one_answer);
         boolean questionOneRight = questionOne.isChecked();
 
-        CheckBox questionTwoAnswerOne = (CheckBox)findViewById(R.id.question_two_answer_one);
+        CheckBox questionTwoAnswerOne = (CheckBox) findViewById(R.id.question_two_answer_one);
         boolean questionTwoRightOne = questionTwoAnswerOne.isChecked();
 
-        CheckBox questionTwoAnswerTwo = (CheckBox)findViewById(R.id.question_two_answer_two);
+        CheckBox questionTwoAnswerTwo = (CheckBox) findViewById(R.id.question_two_answer_two);
         boolean questionTwoRightTwo = questionTwoAnswerTwo.isChecked();
 
-        CheckBox questionTwoAnswerThree = (CheckBox)findViewById(R.id.question_two_answer_three);
+        CheckBox questionTwoAnswerThree = (CheckBox) findViewById(R.id.question_two_answer_three);
         boolean questionTwoRightThree = questionTwoAnswerThree.isChecked();
 
-        RadioButton questionThree = (RadioButton)findViewById(R.id.question_three_answer);
+        RadioButton questionThree = (RadioButton) findViewById(R.id.question_three_answer);
         boolean questionThreeRight = questionThree.isChecked();
 
-        RadioButton questionFour = (RadioButton)findViewById(R.id.question_four_answer);
+        RadioButton questionFour = (RadioButton) findViewById(R.id.question_four_answer);
         boolean questionFourRight = questionFour.isChecked();
 
-        EditText stringNumber = (EditText)findViewById(R.id.question_five_answer);
+        EditText stringNumber = (EditText) findViewById(R.id.question_five_answer);
         int questionFiveRight = Integer.parseInt(stringNumber.getText().toString());
 
 
-        int totalScore = score(questionOneRight,questionTwoRightOne,questionTwoRightTwo,questionTwoRightThree,questionThreeRight, questionFourRight, questionFiveRight);
+        int totalScore = score(questionOneRight, questionTwoRightOne, questionTwoRightTwo, questionTwoRightThree, questionThreeRight, questionFourRight, questionFiveRight);
 
-        String Message = createSummary(totalScore,questionOneRight,questionTwoRightOne,questionTwoRightTwo,questionTwoRightThree,questionThreeRight, questionFourRight, questionFiveRight);
+        String Message = createSummary(totalScore, questionOneRight, questionTwoRightOne, questionTwoRightTwo, questionTwoRightThree, questionThreeRight, questionFourRight, questionFiveRight);
 
         Toast.makeText(MainActivity.this, Message, Toast.LENGTH_LONG).show();
     }
